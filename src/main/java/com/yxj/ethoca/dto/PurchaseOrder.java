739d94187d8f4418e6d6620b7216b40c72c9c047
Yong Jian <yong.jian@scotiabank.com>
2020-06-06 23:57:51 -0400

    added lineitem min size check

--- src/main/java/com/yxj/ethoca/dto/PurchaseOrder.java
+++ src/main/java/com/yxj/ethoca/dto/PurchaseOrder.java
@@ -3,12 +3,17 @@ package com.yxj.ethoca.dto;
 import org.bson.types.ObjectId;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 public class PurchaseOrder {
 
+    @NotNull
     @Valid
+    @Size (min = 1, message = "Line Items must contain at least 1 item")
     private List<LineItem> lineItems; //list of items in this purchase order
     private ObjectId id;
 
